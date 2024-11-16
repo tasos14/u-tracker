f73da0d36ba5e114e74b1aa119e7cac578ccca0a
Tasos <atheodoridis@trigonongroup.com>
2024-11-16 15:16:33 +0200

    Add MonthlyCalendar component for date selection and navigation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,20 +1,17 @@
 import ChartHeader from '@/components/ChartHeader';
 import { Theme } from '@/constants/theme';
 import { getChartData, getDailySummary } from '@/db/db';
-import { Ionicons } from '@expo/vector-icons';
-import { addDays, format, subDays } from 'date-fns';
 import { useFocusEffect } from 'expo-router';
 import { useCallback, useEffect, useState } from 'react';
-import { Pressable, ScrollView, StyleSheet, Text, useColorScheme, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, useColorScheme, View } from 'react-native';
 import { BarChart, PieChart } from 'react-native-gifted-charts';
-import DateTimePicker from '@react-native-community/datetimepicker';
+import MonthlyCalendar from '@/components/MonthlyCalendar';
 
 export default function Index() {
     const [dailySummary, setDailySummary] = useState({ waterIntake: 0, urineLoss: 0, catheterized: 0 });
     const [chartData, setChartData] = useState<{ value: number; label: string; frontColor: string }[]>([]);
     const [totalChartData, setTotalChartData] = useState<{ value: number; label: string; frontColor: string }[]>([]);
     const [date, setDate] = useState(new Date());
-    const [showDate, setShowDate] = useState(false);
     const colorScheme = useColorScheme() || 'light';
 
     const color = Theme[colorScheme].colors.text;
@@ -37,124 +34,87 @@ export default function Index() {
         getData(date);
     }, [date, getData]);
 
-    const handlePreviousDay = () => {
-        setDate((prevDate) => subDays(prevDate, 1));
-    };
-
-    const handleNextDay = () => {
-        setDate((prevDate) => addDays(prevDate, 1));
-    };
-
-    const onDateChange = ({ type }: any, selectedDate: Date | undefined) => {
-        if (type === 'set' && selectedDate) {
-            setDate(selectedDate);
-        }
-        setShowDate(false);
-    };
-
     return (
-        <ScrollView>
-            <View style={styles.dateWrapper}>
-                <Pressable onPress={handlePreviousDay}>
-                    <Ionicons name="arrow-back" size={28} color={color} />
-                </Pressable>
+        <>
+            <MonthlyCalendar date={date} onChange={setDate} />
+            <ScrollView>
+                <Text style={[styles.sectionTitle, { color }]}>Daily Summary</Text>
+                <View style={[styles.totalCard, { backgroundColor }]}>
+                    <Text style={[styles.totalValue, { color }]}>{dailySummary.waterIntake} ml</Text>
+                    <Text style={[styles.totalTitle, { color }]}>Daily Water Intake</Text>
+                </View>
+                <View style={[styles.totalCard, { backgroundColor }]}>
+                    <Text style={[styles.totalValue, { color }]}>{dailySummary.urineLoss} ml</Text>
+                    <Text style={[styles.totalTitle, { color }]}>Daily Urine Loss</Text>
+                </View>
+                <View style={[styles.totalCard, { backgroundColor }]}>
+                    <Text style={[styles.totalValue, { color }]}>{dailySummary.catheterized} ml</Text>
+                    <Text style={[styles.totalTitle, { color }]}>Daily Catheterized</Text>
+                </View>
 
-                <Pressable onPress={() => setShowDate(true)}>
-                    <Text style={[styles.dateText, { color }]}>{format(date, 'iii dd MMM')}</Text>
-                </Pressable>
-                {showDate && <DateTimePicker mode="date" value={date} onChange={onDateChange} />}
-
-                <Pressable onPress={handleNextDay}>
-                    <Ionicons name="arrow-forward" size={28} color={color} />
-                </Pressable>
-            </View>
-
-            <Text style={[styles.sectionTitle, { color }]}>Daily Summary</Text>
-            <View style={[styles.totalCard, { backgroundColor }]}>
-                <Text style={[styles.totalValue, { color }]}>{dailySummary.waterIntake} ml</Text>
-                <Text style={[styles.totalTitle, { color }]}>Daily Water Intake</Text>
-            </View>
-            <View style={[styles.totalCard, { backgroundColor }]}>
-                <Text style={[styles.totalValue, { color }]}>{dailySummary.urineLoss} ml</Text>
-                <Text style={[styles.totalTitle, { color }]}>Daily Urine Loss</Text>
-            </View>
-            <View style={[styles.totalCard, { backgroundColor }]}>
-                <Text style={[styles.totalValue, { color }]}>{dailySummary.catheterized} ml</Text>
-                <Text style={[styles.totalTitle, { color }]}>Daily Catheterized</Text>
-            </View>
-
-            <Text style={[styles.sectionTitle, { color }]}>Daily Summary Chart</Text>
-            <View style={[styles.chartContainer, { backgroundColor }]}>
-                <ChartHeader />
-                <BarChart
-                    data={chartData}
-                    isAnimated
-                    roundedTop
-                    showVerticalLines
-                    verticalLinesColor="rgba(211, 211, 211, 0.1)"
-                    rulesColor="rgba(211, 211, 211, 0.1)"
-                    rulesType="solid"
-                    showGradient
-                    stepValue={100}
-                    noOfSections={8}
-                    barWidth={10}
-                    labelWidth={35}
-                    xAxisColor={color}
-                    yAxisColor={color}
-                    yAxisTextStyle={{ color }}
-                    xAxisLabelTextStyle={{
-                        color,
-                        transform: chartData.length > 0 ? 'rotate(50deg) translate(5px, 5px)' : null,
-                    }}
-                />
-            </View>
-            <View style={[styles.chartContainer, { backgroundColor }]}>
-                <ChartHeader />
-                <View style={styles.doubleChartContainer}>
-                    <View style={styles.chartWrapper}>
-                        <BarChart
-                            data={totalChartData}
-                            isAnimated
-                            roundedTop
-                            showVerticalLines
-                            verticalLinesColor="rgba(211, 211, 211, 0.1)"
-                            rulesColor="rgba(211, 211, 211, 0.1)"
-                            rulesType="solid"
-                            showGradient
-                            stepValue={250}
-                            noOfSections={8}
-                            barWidth={10}
-                            labelWidth={10}
-                            yAxisLabelWidth={40}
-                            xAxisColor={color}
-                            yAxisColor={color}
-                            yAxisTextStyle={{ color }}
-                            xAxisLabelTextStyle={{ color }}
-                        />
-                    </View>
-                    <View style={styles.chartWrapper}>
-                        <PieChart
-                            radius={80}
-                            data={totalChartData.map((item) => ({ value: item.value, color: item.frontColor }))}
-                        />
+                <Text style={[styles.sectionTitle, { color }]}>Daily Summary Chart</Text>
+                <View style={[styles.chartContainer, { backgroundColor }]}>
+                    <ChartHeader />
+                    <BarChart
+                        data={chartData}
+                        isAnimated
+                        roundedTop
+                        showVerticalLines
+                        verticalLinesColor="rgba(211, 211, 211, 0.1)"
+                        rulesColor="rgba(211, 211, 211, 0.1)"
+                        rulesType="solid"
+                        showGradient
+                        stepValue={100}
+                        noOfSections={8}
+                        barWidth={10}
+                        labelWidth={35}
+                        xAxisColor={color}
+                        yAxisColor={color}
+                        yAxisTextStyle={{ color }}
+                        xAxisLabelTextStyle={{
+                            color,
+                            transform: chartData.length > 0 ? 'rotate(50deg) translate(5px, 5px)' : null,
+                        }}
+                    />
+                </View>
+                <View style={[styles.chartContainer, { backgroundColor }]}>
+                    <ChartHeader />
+                    <View style={styles.doubleChartContainer}>
+                        <View style={styles.chartWrapper}>
+                            <BarChart
+                                data={totalChartData}
+                                isAnimated
+                                roundedTop
+                                showVerticalLines
+                                verticalLinesColor="rgba(211, 211, 211, 0.1)"
+                                rulesColor="rgba(211, 211, 211, 0.1)"
+                                rulesType="solid"
+                                showGradient
+                                stepValue={500}
+                                noOfSections={8}
+                                barWidth={10}
+                                labelWidth={10}
+                                yAxisLabelWidth={40}
+                                xAxisColor={color}
+                                yAxisColor={color}
+                                yAxisTextStyle={{ color }}
+                                xAxisLabelTextStyle={{ color }}
+                            />
+                        </View>
+                        <View style={styles.chartWrapper}>
+                            <PieChart
+                                radius={80}
+                                data={totalChartData.map((item) => ({ value: item.value, color: item.frontColor }))}
+                            />
+                        </View>
                     </View>
                 </View>
-            </View>
-        </ScrollView>
+            </ScrollView>
+        </>
     );
 }
 
 const styles = StyleSheet.create({
-    dateWrapper: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        padding: 10,
-    },
-    dateText: {
-        fontSize: 24,
-        fontWeight: 'bold',
-    },
     sectionTitle: {
         fontSize: 16,
         marginBottom: 10,
---
+++ components/MonthlyCalendar.tsx
@@ -0,0 +1,105 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { eachDayOfInterval, endOfMonth, format, getDate, startOfMonth } from 'date-fns';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { Pressable, ScrollView, StyleSheet, Text, TouchableOpacity, useColorScheme, View } from 'react-native';
+import { Theme } from '@/constants/theme';
+
+const getDaysOfCurrentMonth = (date: Date) => {
+    const start = startOfMonth(date);
+    const end = endOfMonth(date);
+    return eachDayOfInterval({ start, end });
+};
+
+function MonthlyCalendar({ date, onChange }: { date: Date; onChange: (date: Date) => void }) {
+    const [showDate, setShowDate] = useState(false);
+    const scrollViewRef = useRef<ScrollView>(null);
+    const days = getDaysOfCurrentMonth(date);
+    const [scrollViewWidth, setScrollViewWidth] = useState(0);
+    const colorScheme = useColorScheme() || 'light';
+
+    const color = Theme[colorScheme].colors.text;
+
+    useEffect(() => {
+        const selectedIndex = days.findIndex((day) => day.getDate() === getDate(date));
+        if (scrollViewRef.current && selectedIndex !== -1) {
+            const itemWidth = 60;
+            const scrollPosition = selectedIndex * itemWidth - scrollViewWidth / 2 + itemWidth / 2;
+            scrollViewRef.current.scrollTo({ x: scrollPosition, animated: false });
+        }
+    }, [days, date, scrollViewWidth]);
+
+    const onDateChange = ({ type }: any, selectedDate: Date | undefined) => {
+        if (type === 'set' && selectedDate) {
+            onChange(selectedDate);
+        }
+        setShowDate(false);
+    };
+
+    return (
+        <View style={styles.container}>
+            <Pressable onPress={() => setShowDate(true)}>
+                <Text style={[styles.dateText, { color }]}>{format(date, 'MMM, yyyy')}</Text>
+            </Pressable>
+            {showDate && <DateTimePicker mode="date" value={date} onChange={onDateChange} />}
+            <ScrollView
+                style={{ position: 'relative' }}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                ref={scrollViewRef}
+                onLayout={(event) => setScrollViewWidth(event.nativeEvent.layout.width)}
+            >
+                {days.map((day) => {
+                    const isSelected = day.getDate() === getDate(date);
+                    return (
+                        <TouchableOpacity onPress={() => onChange(day)} key={day.getDate()} style={[styles.day]}>
+                            <Text style={[styles.dayText, isSelected ? styles.selectedDayText : {}]}>
+                                {day.getDate()}
+                            </Text>
+                            <Text style={[styles.weekdayText, { color }, isSelected ? styles.selectedWeekDayText : {}]}>
+                                {day.toDateString().split(' ')[0]}
+                            </Text>
+                        </TouchableOpacity>
+                    );
+                })}
+            </ScrollView>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        paddingBottom: 10,
+    },
+    dateText: {
+        fontSize: 16,
+        marginTop: 8,
+    },
+    day: {
+        width: 44,
+        marginHorizontal: 8,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    selectedDayText: {
+        fontSize: 26,
+        opacity: 1,
+        fontWeight: 'bold',
+    },
+    dayText: {
+        color: 'white',
+        fontSize: 24,
+        fontWeight: 400,
+        opacity: 0.5,
+    },
+    selectedWeekDayText: {
+        opacity: 1,
+        fontWeight: 'bold',
+    },
+    weekdayText: {
+        fontSize: 18,
+        opacity: 0.5,
+    },
+});
+
+export default MonthlyCalendar;
